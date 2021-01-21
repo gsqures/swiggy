413d47de8e72d23d685a614e5fb8731f1ff786f0
Souvik Ghosh <souvikghosh687@gmail.com>
2021-01-22 00:24:16 +0530

    customized login page

--- app/src/main/java/com/example/swiggy/log_in_page.java
+++ app/src/main/java/com/example/swiggy/log_in_page.java
@@ -14,21 +14,24 @@ public class log_in_page extends AppCompatActivity {
     CarouselView carouselView;
 
     int[] sampleImages = {R.drawable.fp1,R.drawable.fp2,R.drawable.fp3,R.drawable.fp4,R.drawable.fp5};
+    ImageListener imageListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.log_in_page);
+        getSupportActionBar().hide();
+
         //carouseview
         carouselView = findViewById(R.id.carouselView);
         carouselView.setPageCount(sampleImages.length);
+        imageListener = new ImageListener() {
+            @Override
+            public void setImageForPosition(int position, ImageView imageView) {
+                imageView.setImageResource(sampleImages[position]);
+            }
+        };
         carouselView.setImageListener(imageListener);
     }
-    ImageListener imageListener = new ImageListener() {
-        @Override
-        public void setImageForPosition(int position, ImageView imageView) {
-            imageView.setImageResource(sampleImages[position]);
-        }
-    };
 
 }
