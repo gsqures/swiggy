158e773114470782452df956c2ecbcf2b9fce614
soumen <palsoumen306@gmail.com>
2021-01-21 22:26:07 +0530

    add carouse view in login page

---
+++ app/src/main/java/com/example/swiggy/log_in_page.java
@@ -0,0 +1,34 @@
+package com.example.swiggy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toolbar;
+
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
+
+public class log_in_page extends AppCompatActivity {
+
+    CarouselView carouselView;
+
+    int[] sampleImages = {R.drawable.fp1,R.drawable.fp2,R.drawable.fp3,R.drawable.fp4,R.drawable.fp5};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_in_page);
+        //carouseview
+        carouselView = findViewById(R.id.carouselView);
+        carouselView.setPageCount(sampleImages.length);
+        carouselView.setImageListener(imageListener);
+    }
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(sampleImages[position]);
+        }
+    };
+
+}
